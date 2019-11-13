a617e2310aec504269e124b1fbf43eb90f85ea08
Amol <amolpukale23@gmail.com>
2019-11-13 14:42:08 +0530

    server.js added

---
+++ server.js
@@ -0,0 +1,11 @@
+var express=require("express");
+var app=express();
+
+app.get("/",function(request,response){
+    response.send("Welcome");
+})
+
+
+app.listen("4000",function(err,res){
+    res.send("Hi");
+})
