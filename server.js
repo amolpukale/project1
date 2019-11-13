75dfe711789328b5f6edc7074d9d4b2c061f7941
Amol <amolpukale23@gmail.com>
2019-11-13 15:00:14 +0530

    added post

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ app.get("/",function(request,response){
     response.send("Welcome");
 })
 
+app.post("/",function(request,response){
+response.send("in post");
 
 app.listen("4000",function(err,res){
     res.send("Hi");
